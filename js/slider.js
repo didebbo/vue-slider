422da8f2625b1563aef084dabaa68d439a2e3ac7
didebbo <gianluca.napoletano.93@gmail.com>
2021-09-17 15:29:05 +0200

    aggiunto controllo freccie slider

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,35 @@ const root = new Vue(
     {
         el: "#slider",
         data: {
-            debug: "Init Project!"
+            bird: {
+                index: 0,
+                images: ['01', '02', '03', '04', '05'],
+                name: "Bird",
+                src: "images/",
+            }
+        },
+        mounted: function () {
+            this.bird.name += " " + this.bird.images[this.bird.index];
+            this.bird.src += this.bird.images[this.bird.index] + ".jpg";
+        },
+        methods: {
+            activeCircle: function (index) {
+                if (index == this.bird.index) return "active";
+            },
+            renderImage: function () {
+                if (this.bird.index >= this.bird.images.length) this.bird.index = 0;
+                else if (this.bird.index < 0) this.bird.index = this.bird.images.length - 1;
+                this.bird.name = "Bird " + this.bird.images[this.bird.index];
+                this.bird.src = "images/" + this.bird.images[this.bird.index] + ".jpg";
+            },
+            nextImage: function () {
+                this.bird.index++;
+                this.renderImage();
+            },
+            prevImage: function () {
+                this.bird.index--;
+                this.renderImage();
+            }
         }
     }
 );
